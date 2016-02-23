798e7d07fd1c328deb2765f94e9cc79ad2be57c0
Joe Pappas <pappas_joe@yahoo.com>
2016-02-23 16:54:41 -0500

    Added eight class and test file

---
+++ Eight.m
@@ -0,0 +1,131 @@
+classdef Eight < handle
+    %EIGHT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        layout
+        pathCost
+        explored
+    end
+    properties (SetAccess = private)
+        parent = Eight.empty;
+        lastMove
+    end
+    
+    methods
+        function obj = Eight()
+            obj.pathCost = 0;
+            obj.explored = false;
+        end
+        
+        function visitnode(newNode, parent, move)
+            newNode.parent = parent;
+            newNode.lastMove = move;
+            parent.explored = true;
+        end
+        
+        function hasbeenexplored(obj)
+            
+        end
+        
+        function u = canmoveup(obj)
+            [row, col] = find(obj.layout ==0);
+            if (row==2||row==3)
+                u = true;
+            else
+                u = false;
+            end
+        end
+        
+        function d = canmovedown(obj)
+            [row, col] = find(obj.layout ==0);
+            if (row==1||row==2)
+                d = true;
+            else
+                d = false;
+            end
+        end
+        
+        function r = canmoveright(obj)
+            [row, col] = find(obj.layout ==0);
+            if (col==1||col==2)
+                r = true;
+            else
+                r = false;
+            end
+         end
+        
+         function l = canmoveleft(obj)
+            [row, col] = find(obj.layout ==0);
+            if (col==2||col==3)
+                l = true;
+            else
+                l = false;
+            end
+         end
+         
+         function obj2 = moveleft(obj)
+             if canmoveleft(obj)
+                [row, col] = find(obj.layout == 0);
+                numToSwitch = obj.layout(row,col-1);
+                obj2 = Eight;
+                obj2.layout = obj.layout;
+                obj2.layout(row,col) = numToSwitch;
+                obj2.layout(row, col-1) = 0;
+                visitnode(obj2, obj, 'left');
+             end
+         end
+         
+        function obj2 = moveright(obj)
+             if canmoveright(obj)
+                [row, col] = find(obj.layout == 0);
+                numToSwitch = obj.layout(row,col+1);
+                obj2 = Eight;
+                obj2.layout = obj.layout;
+                obj2.layout(row,col) = numToSwitch;
+                obj2.layout(row, col+1) = 0;
+                visitnode(obj2, obj, 'right');
+             end
+        end
+        function obj2 = moveup(obj)
+             if canmoveup(obj)
+                [row, col] = find(obj.layout == 0);
+                numToSwitch = obj.layout(row-1,col);
+                obj2 = Eight;
+                obj2.layout = obj.layout;
+                obj2.layout(row,col) = numToSwitch;
+                obj2.layout(row-1, col) = 0;
+                visitnode(obj2, obj, 'up');
+             end
+        end
+         
+         function obj2 = movedown(obj)
+             if canmovedown(obj)
+                [row, col] = find(obj.layout == 0);
+                numToSwitch = obj.layout(row+1,col);
+                obj2 = Eight;
+                obj2.layout = obj.layout;
+                obj2.layout(row,col) = numToSwitch;
+                obj2.layout(row+1, col) = 0;
+                visitnode(obj2, obj, 'down');
+             end
+         end
+         
+         function delete(obj)
+             clc
+             clear
+         end
+         
+         function x = isGoalState(obj)
+             goalState = [1 2 3; 4 5 6; 7 8 0];
+             if goalState == obj.layout
+                 x = true;
+             else
+                 x = false;
+             end
+         end
+         
+    end
+    
+end
+
---
+++ test.m
@@ -0,0 +1,27 @@
+clc
+clear
+format compact
+state = [1 2 3; 4 5 6; 7 0 8];
+obj = Eight;
+obj.layout = state;
+y = obj.canmoveup()
+a = obj.canmovedown()
+b = obj.canmoveright()
+c = obj.canmoveleft()
+obj7 = obj.moveright()
+state1 = [1 2 3; 4 5 0; 7 8 6];
+obj2 = Eight;
+obj2.layout = state1;
+y = obj2.canmoveup()
+a = obj2.canmovedown()
+b = obj2.canmoveright()
+c = obj2.canmoveleft()
+obj3 = obj2.moveleft()
+obj4 = obj2.moveup()
+obj5 = obj2.movedown()
+state2 = [1 2 3; 4 5 6; 7 8 0];
+objjj = Eight;
+objjj.layout = state2;
+x = objjj.isGoalState
+
+
