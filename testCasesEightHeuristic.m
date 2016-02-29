6499cdb4a1a94595b9ef7d0426971acac35a88b8
Amber Harriger <alharriger@gmail.com>
2016-02-29 17:43:22 -0500

    Test case file for BFS and IDS and the other to create 50 states

--- testCasesEightHeuristic.m
+++ testCasesEightHeuristic.m
@@ -4,31 +4,50 @@
 clc
 clear
 goalState = [1 2 3; 4 5 6; 7 8 0];
-obj = Eight(goalState);
+obj = eightHeuristic(goalState);
 exploredLayouts = [];
 %%generate pathcost = 10 layouts
 finalObjects = [];
 [exploredLayouts, finalObjects] = createTests(obj, exploredLayouts, 10, finalObjects);
 finalObjects = finalObjects(randperm(length(finalObjects)));
-finalTenObjects = finalObjects(1:20);
+finalTenObjects = finalObjects(1:10);
 
 %%generate pathcost = 15 layouts
 finalObjects = [];
 i=1;
-while i < 20
+while i < 10
     [exploredLayouts, finalObjects] = createTests(finalTenObjects(i),  exploredLayouts, 15, finalObjects);
     i = i +1;
 end
 finalObjects = finalObjects(randperm(length(finalObjects)));
-finalFifteenObjects = finalObjects(1:20);
+finalFifteenObjects = finalObjects(1:10);
 
 %%generate pathcost = 20 layouts
 finalObjects = [];
 i = 1;
-while i < 5
+while i < 10
     [exploredLayouts, finalObjects] = createTests(finalFifteenObjects(i),  exploredLayouts, 20, finalObjects);
     i = i +1;
 end
 finalObjects = finalObjects(randperm(length(finalObjects)));
-finalTwentyObjects = finalObjects(1:5);
+finalTwentyObjects = finalObjects(1:10);
 
+%%generate pathcost = 25 layouts
+finalObjects = [];
+i = 1;
+while i < 10
+    [exploredLayouts, finalObjects] = createTests(finalTwentyObjects(i),  exploredLayouts, 25, finalObjects);
+    i = i+1;
+end
+finalObjects = finalObjects(randperm(length(finalObjects)));
+finalTwentyFiveObjects = finalObjects(1:10);
+
+%%generate pathcost = 30 layouts
+finalObjects = [];
+i = 1;
+while i < 10
+    [exploredLayouts, finalObjects] = createTests(finalTwentyFiveObjects(i), exploredLayouts, 30, finalObjects);
+    i = i +1;
+end
+finalObjects = finalObjects(randperm(length(finalObjects)));
+finalThirtyObjects = finalObjects(1:10);
